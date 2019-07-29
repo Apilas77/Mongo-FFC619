2b4de9b3e34a4c1dcfd88756c5c9c35ce3d36edd
jojo <jojo@lapomme.fr>
2019-07-29 14:16:52 +0200

    initialisation Schema

--- myApp.js
+++ myApp.js
@@ -40,8 +40,12 @@ mongoose.connect(process.env.MONGO_URI)
 // `default` values. See the [mongoose docs](http://mongoosejs.com/docs/guide.html).
 
 // <Your code here >
-
-var Person /* = <Your Model> */
+var Schema = mongoose.Schema;
+var Person = new Schema ({
+  name: String,
+  age: Number,
+  favoritefoods: [String]
+});
 
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
